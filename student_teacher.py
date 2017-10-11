fdccd8770389d059b34a7a54810abf3432fcca0b
Chris Closser <chrisclosser1@gmail.com>
2017-10-11 13:35:24 -0700

    tabing

--- student_teacher.py
+++ student_teacher.py
@@ -17,39 +17,55 @@ tyler = {
   "tests": [100.0, 100.0]
 }
 
+
 def average(numbers):
-  total = 0
-  total = sum(numbers)
-  total = float(total) / len(numbers)
-  return total
+    total = 0
+    total = sum(numbers)
+    total = float(total) / len(numbers)
+    return total
+
 
 def get_average(student):
-  homework = average(student['homework'])
-  quizzes = average(student['quizzes'])
-  tests = average(student['tests'])
-  homework = homework * 0.10
-  quizzes = quizzes * 0.3
-  tests = tests * 0.6
-  total = homework + quizzes + tests
-  print student['name']
-  print total
-  return total
+    homework = average(student['homework'])
+    quizzes = average(student['quizzes'])
+    tests = average(student['tests'])
+    homework = homework * 0.10
+    quizzes = quizzes * 0.3
+    tests = tests * 0.6
+    total = homework + quizzes + tests
+    return total
+
 
 def get_letter_grade(score):
-  if score >= 90:
-    return 'A'
-  elif score >= 80:
-    return 'B'
-  elif score >= 70:
-    return 'C'
-  elif score >= 60:
-    return 'D'
-  else:
-  	return 'F'
+    if score >= 90:
+        return 'A'
+    elif score >= 80:
+        return 'B'
+    elif score >= 70:
+        return 'C'
+    elif score >= 60:
+        return 'D'
+    else:
+        return 'F'
+
 
 first_class = [lloyd, alice, tyler]
 
 for student in first_class:
-	first_quarter = get_average(student)
-	print get_letter_grade(first_quarter)
-  	print
+    first_quarter = get_average(student)
+    print student['name']
+    print first_quarter
+    print get_letter_grade(first_quarter)
+    print
+
+
+def get_class_average(class_list):
+    results = []
+    for student in class_list:
+        grade = get_average(student)
+        results.append(grade)
+        class_avg = average(results)
+        return class_avg
+
+
+print 'Class average: %s' % get_class_average(first_class)
