3137756dda0409d5206340aa690302421f3c108d
Chris Closser <chrisclosser1@gmail.com>
2017-10-12 16:10:22 -0700

    more handy stuff

--- cool_stuff.py
+++ cool_stuff.py
@@ -157,3 +157,21 @@ def remove_duplicates(inputlist):
 
 
 print remove_duplicates([1, 4, 5, 2, 3, 1, 3, 1, 4, 2, 3, 5, 5])
+
+
+# finds the middle of a list of numbers
+def median(lst):
+    sorted_list = sorted(lst)
+    if len(sorted_list) % 2 != 0:
+        # odd number of elements
+        index = len(sorted_list) // 2
+        return sorted_list[index]
+    elif len(sorted_list) % 2 == 0:
+        # even number of elements
+        index_1 = len(sorted_list) / 2 - 1
+        index_2 = len(sorted_list) / 2
+        mean = (sorted_list[index_1] + sorted_list[index_2]) / 2.0
+        return mean
+
+
+print median([2, 4, 5, 9])
--- student_teacher.py
+++ student_teacher.py
@@ -82,3 +82,44 @@ print 'Class average: %s' % first_class_avg
 
 class_letter = get_letter_grade(first_class_avg)
 print class_letter
+
+
+# grade calculator
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+
+def print_grades(grades_input):
+    for grade in grades_input:
+        print grade
+
+
+def grades_sum(scores):
+    total = 0
+    for score in scores:
+        total += score
+        return total
+
+
+def grades_average(grades_input):
+    sum_of_grades = grades_sum(grades_input)
+    average = sum_of_grades / float(len(grades_input))
+    return average
+
+
+def grades_variance(grades):
+    variance = 0
+    for number in grades:
+        variance += (grades_average(grades) - number) ** 2
+    return variance / len(grades)
+
+
+def grades_std_deviation(variance):
+    return variance ** 0.5
+
+
+variance = grades_variance(grades)
+print_grades(grades)
+print 'Sum:', grades_sum(grades)
+print 'Average:', grades_average(grades)
+print 'Grade variance:', grades_variance(grades)
+print 'Standard deviation:', grades_std_deviation(variance)
